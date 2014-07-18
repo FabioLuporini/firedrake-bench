3255a1dc6e242b3ade8160339f5de429e87535e1
Fabio Luporini <f.luporini12@imperial.ac.uk>
2014-07-18 10:48:43 +0100

    Run ffc's -O in the coffee's test suite

--- forms/firedrake_forms_coffee.py
+++ forms/firedrake_forms_coffee.py
@@ -8,14 +8,17 @@ class FiredrakeFormsCoffee(FiredrakeForms):
     series = {}
     params = [('q', [1, 2, 3, 4]),
               ('p', [1, 2, 3, 4]),
-              ('form', ['mass', 'elasticity', 'mixed_poisson', 'poisson']),
-              ('opt', [(0, False, False), (1, True, False), (0, False, True)])
+              ('form', ['mass', 'elasticity', 'mixed_poisson', 'poisson', 'helmholtz']),
+              ('opt', [(0, False, False, False), (0, False, False, True),
+                       (1, True, False, False), (0, False, True, False)])
               ]
 
-    def forms(self, q=1, p=1, dim=3, max_nf=3, form='mass', opt=(0, False, False)):
+    def forms(self, q=1, p=1, dim=3, max_nf=3, form='mass', opt=(0, False, False, False)):
         parameters["coffee"]["licm"] = opt[0]
         parameters["coffee"]["ap"] = opt[1]
         parameters["coffee"]["autotune"] = opt[2]
+        parameters["coffee"]["ffc-opt"] = opt[3]
+        parameters["form_compiler"]['optimize'] = opt[3]
         super(FiredrakeFormsCoffee, self).forms(q, p, dim, max_nf, form)
 
 if __name__ == '__main__':
@@ -30,17 +33,17 @@ if __name__ == '__main__':
     # Plot
     regions = ['nf %d' % i for i in range(4)]
     b.plot(xaxis='opt', regions=regions, kinds='bar,barlog',
-           xlabel='COFFEE Optimisations (BASE, LICM1, AUTO)',
-           xvalues=['0/n/n', '1/y/n', '0/n/y'],
+           xlabel='COFFEE Optimisations (BASE, FFC-OPT, LICM1, AUTO)',
+           xvalues=['0/n/n/n', '0/n/n/y', '1/y/n/n', '0/n/y/n'],
            title='%(form)s (single core, 3D, degree q = %(q)d, premultiplying degree p = %(p)d)')
     b.plot(xaxis='opt', regions=regions, kinds='bar',
-           xlabel='COFFEE Optimisations (BASE, LICM1, AUTO)',
-           ylabel='Speedup over unoptimised baseline', speedup=((0, False, False),),
-           xvalues=['0/n/n', '1/y/n', '0/n/y'],
+           xlabel='COFFEE Optimisations (BASE, FFC-OPT, LICM1, AUTO)',
+           ylabel='Speedup over unoptimised baseline', speedup=((0, False, False, False),),
+           xvalues=['0/n/n/n', '0/n/n/y', '1/y/n/n', '0/n/y/n'],
            title='%(form)s (single core, 3D, degree q = %(q)d, premultiplying degree p = %(p)d)')
     for i, r in enumerate(regions):
         b.plot(xaxis='opt', regions=[r], kinds='bar,barlog',
-               xlabel='COFFEE Optimisations (BASE, LICM1, AUTO)', groups=['form'],
+               xlabel='COFFEE Optimisations (BASE, FFC-OPT, LICM1, AUTO)', groups=['form'],
                figname='FiredrakeFormsCoffee_nf%d' % i,
-               xvalues=['0/n/n', '1/y/n', '0/n/y'],
+               xvalues=['0/n/n/n', '0/n/n/y', '1/y/n/n', '0/n/y/n'],
                title=str(i) + ' premultiplying functions (single core, 3D, degree q = %(q)d, premultiplying degree p = %(p)d)')
