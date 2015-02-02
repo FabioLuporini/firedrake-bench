c32257630f0cd6ce24b6d253219060ea749a0450
Michael Lange <michael.lange@imperial.ac.uk>
2015-02-02 10:34:45 +0000

    Meshing: Measure partition/migration times for each stage

--- meshing/dmplex_meshing.py
+++ meshing/dmplex_meshing.py
@@ -3,8 +3,12 @@ from firedrake import op2
 from firedrake.petsc import PETSc
 
 regions = ['Generate', 'Distribute', 'Refine', 'DistributeOverlap']
-petsc_events = ['Mesh Partition', 'Mesh Migration']
-regions += petsc_events
+petsc_events = { 'Distribute': ['Mesh Partition', 'Mesh Migration'],
+                 'Overlap' : ['Mesh Partition', 'Mesh Migration']}
+
+for stage, events in petsc_events.iteritems():
+    for event in events:
+        regions += "%s::%s" % (stage, event)
 
 class DMPlexMeshing(Meshing):
     benchmark = 'DMPlex_UnitMesh'
@@ -15,6 +19,8 @@ class DMPlexMeshing(Meshing):
 
     def meshing(self, size=32, degree=1, dim=2, fs='scalar', refine=0):
         log = PETSc.Log()
+        stage_dist = log.Stage("Distribute")
+        stage_ol = log.Stage("Overlap")
         log.begin()
 
         with self.timed_region('Generate'):
@@ -27,7 +33,9 @@ class DMPlexMeshing(Meshing):
             plex = PETSc.DMPlex().generate(boundary)
 
         with self.timed_region('Distribute'):
+            stage_dist.push()
             plex.distribute(overlap=0)
+            stage_dist.pop()
 
         with self.timed_region('Refine'):
             plex.setRefinementUniform(True)
@@ -35,11 +43,15 @@ class DMPlexMeshing(Meshing):
                 plex = plex.refine()
 
         with self.timed_region('DistributeOverlap'):
+            stage_ol.push()
             plex.distributeOverlap(overlap=1)
+            stage_ol.push()
 
-        for eventname in petsc_events:
-            info = log.Event(eventname).getPerfInfo()
-            self.register_timing(eventname, info['time'])
+        for stage, events in petsc_events.iteritems():
+            for event in events:
+                info = log.Event(event).getPerfInfo(log.Stage(stage))
+                self.register_timing("%s::%s" % (stage, event), info['time'])
+
 
 if __name__ == '__main__':
     op2.init(log_level='WARNING')
