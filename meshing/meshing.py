fea073d3184bed87870160d8ef2f2efaa9a29707
Michael Lange <michael.lange@imperial.ac.uk>
2015-03-12 05:11:16 +0000

    Meshing: Add plotting options for parallel refinement plots

--- meshing/meshing.py
+++ meshing/meshing.py
@@ -26,12 +26,9 @@ class Meshing(Benchmark):
         self.meta['refine'] = args.refine
         self.series = {'dim' : self.meta['dim'],
                        'np': op2.MPI.comm.size,
-                       'refine': self.meta['refine'],
                        'variant': args.branch}
-        self.params = [('dim', [self.meta['dim']]),
-                       ('size', self.meta['sizes']),
-                       ('refine', [self.meta['refine']]),
-                       ('fs', ['scalar'])]
+        self.params = [('size', self.meta['sizes']),
+                       ('refine', self.meta['refine'])]
 
     def parser(self, **kwargs):
         p = super(Meshing, self).parser(**kwargs)
@@ -41,7 +38,7 @@ class Meshing(Benchmark):
                        help='Mesh sizes to use')
         p.add_argument('--branch', default='master',
                        help='PETSc branch used')
-        p.add_argument('--refine', type=int, default=0,
+        p.add_argument('--refine', type=int, default=[0], nargs='+',
                        help='Refine level (regular)')
         return p
 
@@ -53,23 +50,76 @@ if __name__ == '__main__':
                    help='mesh sizes to plot')
     p.add_argument('--branch', nargs='+', default=['master'],
                    help='PETSc branches to plot')
+    p.add_argument('--refine', type=int, default=[0], nargs='+',
+                   help='Refine level (regular)')
     args = p.parse_args()
     variants = args.branch or ['master']
     groups = ['variant'] if len(args.branch) > 1 else []
 
     b = Meshing(resultsdir=args.resultsdir, plotdir=args.plotdir)
     if args.weak:
-        b.combine_series([('np', args.weak), ('dim', [args.dim]), ('variant', variants)],
-                         filename='DMPlex_UnitMesh')
+        b.combine_series([('np', args.weak), ('dim', [args.dim]), ('variant', variants),
+                          ('refine', args.refine)], filename='DMPlex_UnitMesh')
         b.plot(xaxis='size', regions=regions, xlabel='Mesh size (cells)', groups=groups,
                xvalues=num_cells[b.meta['dim']](b.meta['sizes']),
                figname='FixedRanks', wscale=0.7, format='pdf',
                title='DMPlex_UnitMesh: dim=%(dim)d, nprocs=%(np)d' )
 
-    if args.parallel:
+    if len(args.refine) > 1:
+        import matplotlib.pyplot as plt
+        import matplotlib as mpl
+        from os import path
+        import numpy as np
+
+        b.combine_series([('np', args.parallel), ('dim', [args.dim]),
+                          ('variant', variants)], filename='DMPlex_UnitMesh')
+
+        # Create a figure
+        figsize = (9, 6)
+        fig = plt.figure("DMPlexDistribute_refine.pdf", figsize=figsize, dpi=300)
+        ax = fig.add_subplot(111)
+        # Precompute colormap
+        cmap = mpl.cm.get_cmap("Set1")
+        colors = [cmap(i) for i in np.linspace(0, 0.9, len(regions)*len(args.refine))]
+
+        # Manually overlay multiple subplots to insert group-subsets,
+        # ie. combinations of size/refine that create the same mesh
+        for i, r in enumerate(args.refine):
+            regionstyles = {'Distribute': {'linestyle': 'solid',
+                                           'color': colors[i]},
+                            'DistributeOverlap': {'linestyle': 'dashed',
+                                                  'color': colors[i]},
+                            'Generate': {'linestyle': 'dashdot',
+                                         'color': colors[i]}}
+            if r > 0:
+                regionstyles['Refine'] = {'linestyle': 'dotted',
+                                          'color': colors[i]}
+
+            regions = regionstyles.keys()
+
+            m = args.size[0] / 2**r
+            params = {'dim': args.dim, 'variant': 'master'}
+            groups = {'refine': [r], 'size': [m]}
+            labels = {(r, m): "size %d, refine %d" % (m, r)}
+            b.subplot(ax, xaxis='np', kind='loglog', xvals=args.parallel,
+                      xlabel='Number of processors', xticklabels=args.parallel,
+                      title='DMPlexDistribute with Parallel Refinement',
+                      regions=regions, groups=groups, params=params,
+                      plotstyle=regionstyles, axis='tight',
+                      labels='long', legend={'loc': 'best'})
+
+        fname = "ParallelRefine_loglog_dim%d_variant%s.pdf" % (args.dim, 'master')
+        fpath = path.join(args.plotdir, fname)
+        fig.savefig(path.join(args.plotdir, fname),
+                    orientation='landscape', format="pdf",
+                    transparent=True, bbox_inches='tight')
+
+    elif args.parallel:
         b.combine_series([('np', args.parallel), ('dim', [args.dim]), ('variant', variants)],
                          filename='DMPlex_UnitMesh')
+
         b.plot(xaxis='np', regions=regions, groups=groups, kinds='plot,loglog',
                xlabel='Number of processors', xticklabels=args.parallel,
+               colormap = "Set1", axis='tight',
                figname='DMPlexDistribute', wscale=0.7, format='pdf',
                title='DMPlex_UnitMesh: dim=%(dim)d, size=2^%(size)d')
